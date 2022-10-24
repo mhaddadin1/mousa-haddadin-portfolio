251d999b6a2d6868b22b057eaf4e77dd886eab6a
mousa haddadin <mousahaddadin@gmail.com>
2022-10-23 20:57:32 -0500

    github pages fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/portfolio" element={<Home />}></Route>
+        <Route
+          exact
+          path="/mousa-haddadin-portfolio"
+          element={<Home />}
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
